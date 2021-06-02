312f32de45b9c680761125b2047bd0d9e997cfa8
Sergey Patrushev <79759732+NoSiReapo@users.noreply.github.com>
2021-06-02 19:26:27 +0300

    Add files via upload

---
+++ LW3/Task1/Index.php
@@ -0,0 +1,10 @@
+<?php
+    function  getGETParameter(string $parameter): ?string
+    {
+        return isset($_GET[$parameter])?(string)$_GET[$parameter]: null; 
+    }
+
+    $string1 = getGETParameter("text");
+	$string2 = preg_replace("/\s+/", " ", $string1);
+	$string3 = trim($string2, " ");
+	echo $string3;
---
+++ LW3/Task2/Index.php
@@ -0,0 +1,16 @@
+<?php
+function getQueryStringParameter(string $name): ?string
+{
+    return isset($_GET[$name]) ? $_GET[$name] : null;
+}
+
+$patern_identifier = '/^[a-zA-z][a-zA-z\d]*$/';
+$identifier = getQueryStringParameter('identifier');
+if (preg_match($patern_identifier, $identifier))
+{
+    echo("This is an identifier");
+}
+else
+{
+    echo('This is not an identifier');
+}
